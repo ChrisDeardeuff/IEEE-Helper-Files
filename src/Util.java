445340b33454ab7e84022775eaf7cd0c7448f673
kristoff <chris.deardeuff1131@gmail.com>
2020-10-01 00:28:09 -0400

    Added two Binary Searches

---
+++ src/Util.java
@@ -0,0 +1,49 @@
+public final class Util {
+
+    private Util(){
+
+    }
+
+    /**
+     * print call println and allows the user to not have to type System.out everytime to print
+     *
+     * @param s the object passed to be printed
+     */
+    public static void print(Object s){
+        System.out.println(s);
+    }
+
+    //================================================
+    //====================Searches====================
+    //================================================
+
+    /**
+     * Perform a Binary Search through an Integer Array.
+     * @param intArray array to be searched.
+     * @param value the value to be searched for.
+     * @return index, the index of the value if located in array. If not then return -1
+     */
+    public static int binarySearch(int[] intArray, int value){
+        int index = -1;
+        //Implement Binary Search for integer here. Update index with the correct index if found.
+        //If not fount return index as -1;
+
+
+        return index;
+    }
+
+    /**
+     * Perform a Binary Search through a String Array.
+     * @param sArray array to be searched.
+     * @param value the value to be searched for.
+     * @return index, the index of the value if located in array. If not then return -1
+     */
+    public static int binarySearch(String[] sArray, String value){
+        int index = -1;
+        //Implement Binary Search for String here. Update index with the correct index if found.
+        //If not fount return index as -1;
+
+
+        return index;
+    }
+}
